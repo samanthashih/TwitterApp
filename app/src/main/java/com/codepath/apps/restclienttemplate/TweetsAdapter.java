afc95a6fb32c6e593964ff0042ef42dde1cf9f98
Samantha Shih <sam.shih21@gmail.com>
2022-06-10 16:11:48 -0700

    fixed syntax

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -112,7 +112,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             Glide.with(context).load(tweet.user.profileImageUrl).transform(new RoundedCorners(100)).into(ivProfileImage);
 
             // if tweet has an image, add it
-            if (!tweet.mediaImageUrl.euqals("")) {
+            if (!tweet.mediaImageUrl.equals("")) {
                 ivMedia.setVisibility(View.VISIBLE);
 //                Log.i("media", tweet.body  + " media link: " + tweet.mediaImageUrl);
                 Glide.with(context).load(tweet.mediaImageUrl).transform(new RoundedCorners(70)).into(ivMedia);
